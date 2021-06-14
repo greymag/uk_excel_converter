0248ed24d8a16a1ad2887827f5c23809bc038368
greymag <greymag@gmail.com>
2021-06-14 14:38:56 +0300

    Конвертация всей директории. Аргументы для команды.

--- bin/uk_excel_converter.dart
+++ bin/uk_excel_converter.dart
@@ -1,19 +1,38 @@
+import 'dart:io';
+
 import 'package:uk_excel_converter/uk_excel_converter.dart';
 import 'package:path/path.dart' as p;
 
 void main(List<String> arguments) async {
-  // loadServiceMapFromExcel(
-  // '/Volumes/MacbookExt/projects/dart/uk_excel_converter/Соответствие услуг.xlsx');
-  final input =
-      '/Volumes/MacbookExt/projects/dart/uk_excel_converter/Октябрь 2020.xlsx';
-  final lsMapFilePath =
-      '/Volumes/MacbookExt/projects/dart/uk_excel_converter/Соответствие ЛС и СЧЕТА.xlsx';
-  final providersMapFilePath =
-      '/Volumes/MacbookExt/projects/dart/uk_excel_converter/Номер поставщика для КАП_РЕМОНТА ВСЕ.xlsx';
-  await convert(
-    input,
-    p.dirname(input),
-    lsMapFilePath: lsMapFilePath,
-    providersMapFilePath: providersMapFilePath,
-  );
+  if (arguments.length != 4) {
+    _usage();
+    return;
+  }
+
+  final sourceDirPath = arguments[0];
+  final targetDirPath = arguments[1];
+  final lsMapFilePath = arguments[2];
+  final providersMapFilePath = arguments[3];
+
+  final dir = Directory(sourceDirPath);
+  await for (final file in dir.list()) {
+    if (file is File && p.extension(file.path) == '.xlsx') {
+      print('Exporting ${p.basename(file.path)}');
+      final path = file.path;
+      await convert(
+        path,
+        targetDirPath,
+        lsMapFilePath: lsMapFilePath,
+        providersMapFilePath: providersMapFilePath,
+      );
+    }
+  }
+
+  print('All done');
 }
+
+void _usage() => print('Запустите команду со следующими аргументами:\n'
+    '- Путь до директории с файлами для экспорта\n'
+    '- Путь до директории куда будут сохранены экспортированные файлы\n'
+    '- Путь до файла соответствия ЛС и счета\n'
+    '- Путь до файла поставщиков');
